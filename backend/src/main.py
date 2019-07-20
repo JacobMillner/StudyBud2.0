ac7550b246e9bb7566a540808e527a0dc402db99
JacobMillner <jacob.millner@outlook.com>
2019-07-20 19:13:09 -0400

    enabling CORS

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from .entities.entity import Session, engine, Base
 from .entities.subject import Subject
 
 # creating the Flask application
 app = Flask(__name__)
+CORS(app)
 
 # generate database schema
 Base.metadata.create_all(engine)
