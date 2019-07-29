01266e23e1c327fc88473510015640cfda03c198
JacobMillner <jacob.millner@outlook.com>
2019-07-29 08:50:28 -0400

    fixing missing import

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from .entities.entity import Session, engine, Base
-from .entities.subject import Subject
+from .entities.subject import Subject, SubjectSchema
 
 # creating the Flask application
 app = Flask(__name__)
