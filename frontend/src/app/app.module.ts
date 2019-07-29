f694a76692844767ed03a95e14e476f06d8b5cad
JacobMillner <jacob.millner@outlook.com>
2019-07-29 19:51:23 -0400

    fixing some bugs

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SubjectsApiService} from './subjects/subjects-api.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [SubjectsApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
